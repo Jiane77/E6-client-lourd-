f8ddd78358ecfd3520708b38a681ddf7e5cb77e6
Jiane <haroundjihane0@gmail.com>
2026-04-06 17:09:44 +0200

    Update PanelPrincipal UI styling and title positioning

--- src/vue/PanelPrincipal.java
+++ src/vue/PanelPrincipal.java
@@ -10,17 +10,18 @@ public abstract class PanelPrincipal  extends JPanel
 {
 	//panel generique qui sert de base pour creer les autres apnels 
 	//garder les caracteristiques communes 
+	
 public PanelPrincipal(String titre) {
-	this.setBackground(Color.gray);
-	this.setBounds(10,80,960,400);
+	this.setBackground(Color.decode("#165763"));
+	this.setBounds(10, 80, 960, 400);
 	this.setLayout(null);
 	JLabel lbTitre =new JLabel(titre);
-	lbTitre.setBounds(240,10,400,20);
+	lbTitre.setBounds(240,20,400,20);
 	this.add(lbTitre);
 	Font unePolice=new Font ("Arial",Font.BOLD,18);
 	lbTitre.setFont(unePolice);
 	lbTitre.setBounds(340,10,400,20);
 	
-	this.setVisible(false);
+	this.setVisible(true);
 }
 }
